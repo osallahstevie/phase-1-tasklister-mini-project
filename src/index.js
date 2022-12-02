9091f976c496e58608f64a8679b941ad264bde0d
osallahstevie <osallahstevie@gmail.com>
2022-12-02 15:15:06 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ let form = document.querySelector('form')
+ form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  buildTaskListerLite(e.target.new_task_description.value)
+  form.reset
+ })
 });
+
+function buildTaskListerLite(createtask) {
+    let p = document.createElement('li')
+    let btn = document.createElement('button')
+    btn.textContent = 'x'
+    p.textContent = createtask
+    p.appendChild(btn)
+    console.log(p)
+    document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelete(e) {
+    e.target.parentNode.remove()
+}
